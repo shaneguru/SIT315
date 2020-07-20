323b72599191f6712da18bb343f53f13938fbdef
Shane Gurusingha <42903047+shaneguru@users.noreply.github.com>
2020-07-20 17:11:46 +1000

    Task 2 with interrupts

---
+++ Module1/Task1.2P.ino
@@ -0,0 +1,21 @@
+int sensor = 2;              // the pin that the sensor is atteched to
+const int buzzer = 7;  // the pin that the buzzer is atteched to
+
+void setup() {  
+  pinMode(buzzer, OUTPUT);
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(9600);        // initialize serial
+  attachInterrupt(digitalPinToInterrupt(sensor), motion, RISING);  //Fires motion when sensor rises from 0 to 1
+}
+
+void loop(){
+  delay(200);
+}
+
+void motion(){
+  Serial.println("Motion detected!");
+  digitalWrite(buzzer, HIGH);
+  noTone(buzzer);     // Stop sound...
+  Serial.println("Buzzed!");
+
+}
