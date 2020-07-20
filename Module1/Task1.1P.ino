85c18be124057581a5c788165decd752ff3edd9b
Shane Gurusingha <42903047+shaneguru@users.noreply.github.com>
2020-07-20 17:04:29 +1000

    Update Task1.1P.ino
    Commented

--- Module1/Task1.1P.ino
+++ Module1/Task1.1P.ino
@@ -1,11 +1,11 @@
 int sensor = 2;              // the pin that the sensor is atteched to
-const int buzzer = 7;
+const int buzzer = 7;  // the pin that the buzzer is atteched to
 
 void setup() {  
   pinMode(buzzer, OUTPUT);
   pinMode(sensor, INPUT);    // initialize sensor as an input
   Serial.begin(9600);        // initialize serial
-  attachInterrupt(digitalPinToInterrupt(sensor), motion, RISING);
+  attachInterrupt(digitalPinToInterrupt(sensor), motion, RISING); //Fires motion when sensor rises from 0 to 1
 }
 
 void loop(){
